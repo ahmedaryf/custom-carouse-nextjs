342aee9d2b7b180e622d3e29ea31cc56614f0a08
ahmedaryf <ahmed.areef@hotmail.com>
2023-08-31 18:06:24 +0500

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
 "use client";
-import { MotionConfig, motion } from "framer-motion";
+import { AnimatePresence, MotionConfig, motion } from "framer-motion";
 import Image from "next/image";
 import { useState } from "react";
-import {
-  BiSolidChevronLeftCircle,
-  BiSolidChevronRightCircle,
-} from "react-icons/bi";
+import { BiSolidChevronLeft, BiSolidChevronRight } from "react-icons/bi";
 
 const images = [
   "/images/image5.jpg",
@@ -16,6 +13,7 @@ const images = [
 
 export default function Home() {
   const [current, setCurrent] = useState(0);
+  const [isFocus, setIsFocus] = useState(false);
 
   // Function to navigate to previous image
   const onPrevClick = () => {
@@ -32,22 +30,27 @@ export default function Home() {
   };
   return (
     <main className='flex flex-col'>
-      <MotionConfig transition={{ duration: 2, ease: [0.32, 0.72, 0, 1] }}>
-        <h1 className='text-4xl text-center font-bold mt-5'>Home Page</h1>
-        <div className='flex flex-col justify-between items-center p-24'>
+      <MotionConfig>
+        <h1 className='text-4xl text-center font-bold mt-5'>
+          Next js Carousel
+        </h1>
+        <div className='flex flex-col justify-between items-center p-4 md:p-24'>
           <div className='relative w-full max-w-[1000px] flex items-center overflow-hidden'>
-            <div className='flex absolute items-center justify-between z-20 left-6 right-6'>
-              <BiSolidChevronLeftCircle
+            <div className='flex absolute items-center justify-between z-20 left-2 right-2'>
+              <BiSolidChevronLeft
                 onClick={onPrevClick}
                 className='text-3xl text-white cursor-pointer'
               />
-              <BiSolidChevronRightCircle
+              <BiSolidChevronRight
                 onClick={onClick}
                 className='text-3xl text-white cursor-pointer'
               />
             </div>
             <motion.div
               animate={{ x: `calc(-${current * 100}% - ${current}rem)` }}
+              transition={{ duration: 2, ease: [0.32, 0.72, 0, 1] }}
+              onHoverStart={() => setIsFocus(true)}
+              onHoverEnd={() => setIsFocus(false)}
               className='flex flex-nowrap gap-4'>
               {images.map((image, index) => {
                 return (
@@ -62,20 +65,42 @@ export default function Home() {
                 );
               })}
             </motion.div>
-          </div>
-          <div className='absolute bottom-10 left-1/2 transform -translate-x-1/2'>
-            <div className='flex px-3 gap-3 py-2 rounded-full bg-slate-200'>
-              {images.map((_, index) => {
-                return (
-                  <button key={index} onClick={() => setCurrent(index)}>
-                    <div
-                      className={`h-2 w-2 bg-slate-400 rounded-full ${
-                        index === current ? "bg-orange-700" : "bg-slate-500"
-                      }`}></div>
-                  </button>
-                );
-              })}
-            </div>
+            <AnimatePresence>
+              {isFocus && (
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 2, ease: [0.32, 0.72, 0, 1] }}
+                  onHoverStart={() => setIsFocus(true)}
+                  onHoverEnd={() => setIsFocus(false)}
+                  className='absolute bottom-2 md:bottom-6 left-1/2 transform -translate-x-1/2'>
+                  <div className='flex px-4 gap-3 py-2 rounded-lg bg-black/40 shadow-lg'>
+                    {images.map((image, index) => {
+                      return (
+                        <button key={index} onClick={() => setCurrent(index)}>
+                          <div className={`w-14 rounded relative shadow-lg`}>
+                            {
+                              <Image
+                                src={image}
+                                alt={image}
+                                width={600}
+                                height={600}
+                                className={`rounded object-cover ${
+                                  current === index
+                                    ? "opacity-40 cursor-default"
+                                    : "opacity-100"
+                                }`}
+                              />
+                            }
+                          </div>
+                        </button>
+                      );
+                    })}
+                  </div>
+                </motion.div>
+              )}
+            </AnimatePresence>
           </div>
         </div>
       </MotionConfig>
